4735b4c5430e90007cea4651a23160e8e1e295c8
Vikash Badal <vikashb@where-ever.za.net>
2025-06-06 15:21:28 +0200

    add all outposts

--- terraform/authentik/outpost.tf
+++ terraform/authentik/outpost.tf
@@ -1,9 +1,13 @@
 resource "authentik_outpost" "embedded_outpost" {
   name = "authentik Embedded Outpost"
+
   protocol_providers = [
-    authentik_provider_proxy.httpbin.id
+    for p in authentik_provider_proxy.proxy_providers : p.id
   ]
 
   service_connection = var.service_connection
-}
 
+  depends_on = [
+    authentik_provider_proxy.proxy_providers
+  ]
+}
