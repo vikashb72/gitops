d960d94a2e3288af00832ab022584a544a42c4a7
Vikash Badal <vikashb@where-ever.za.net>
2025-06-07 11:20:29 +0200

    use users from tfvars

--- terraform/authentik/users.tf
+++ terraform/authentik/users.tf
@@ -1,38 +1,15 @@
-resource "authentik_user" "vikashbadal" {
-  username  = "vikash.badal"
-  name      = "Vikash Badal"
-  email     = "vikashb@minikube.where-ever.net"
+resource "authentik_user" "users" {
+  for_each = { for u in var.users : u.username => u }
+
+  username  = each.value.username
+  name      = each.value.name
+  email     = each.value.email
   is_active = true
-  groups    = concat(
-    [ data.authentik_group.admins.id ],
-    [ for g in authentik_group.groups : g.id ],
-    [ for p in authentik_group.proxy_groups : p.id ]
-  )
-#  groups    = [
-#    data.authentik_group.admins.id,
-#  ]
+  groups    = each.value.groups
 
   depends_on = [
     data.authentik_group.admins,
-    authentik_group.groups,
+    authentik_group.oauth2_groups,
     authentik_group.proxy_groups
   ]
-}
-
-resource "authentik_user" "vikashb" {
-  username  = data.authentik_user.vikashb.name
-  name      = "Vikash Badal"
-  email     = "vikashb@home.where-ever.net"
-  is_active = true
-  groups    = concat(
-    [ data.authentik_group.admins.id ],
-    [ for g in authentik_group.groups : g.id ],
-    [ for p in authentik_group.proxy_groups : p.id ]
-  )
-#  groups    = [
-#    data.authentik_group.admins.id,
-#    authentik_group.argocd_admins.id,
-#    authentik_group.grafana_admins.id,
-#    authentik_group.httpbin_admins.id
-#  ]
 }
