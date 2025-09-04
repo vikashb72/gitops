8c5ee2f529be3845c1081372218d13915fd5fc19
Vikash Badal <vikashb@where-ever.za.net>
2025-09-04 09:06:40 +0200

    update endpoints

--- terraform/zitadel/application_oidc.tf
+++ terraform/zitadel/application_oidc.tf
@@ -3,10 +3,10 @@ resource "zitadel_application_oidc" "default" {
   org_id     = data.zitadel_org.default.id
 
   name                         = "oidc-proxy"
-  redirect_uris                = ["http://127.0.0.1:80/oauth2/callback"]
+  redirect_uris                = ["http://127.0.0.1:4180/oauth2/callback"]
   response_types               = ["OIDC_RESPONSE_TYPE_CODE"]
   grant_types                  = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
-  post_logout_redirect_uris    = ["https://localhost.com"]
+  post_logout_redirect_uris    = ["https://127.0.0.1:4180/oauth2/sign_out"]
   app_type                     = "OIDC_APP_TYPE_WEB"
   auth_method_type             = "OIDC_AUTH_METHOD_TYPE_BASIC"
   version                      = "OIDC_VERSION_1_0"
